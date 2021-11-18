7ea5aaea7fad88dac0d2094cfd9837924c64c42e
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-11-18 11:44:39 -0500

    start project

---
+++ src/bootstrap.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+import "./style/main.scss";
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        
+          <Layout>
+            <p>hey ths is child components</p>
+            <h2>hey there</h2>
+          </Layout>
+      </BrowserRouter>
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
+}
+
+document.addEventListener("DOMContentLoaded", main);
