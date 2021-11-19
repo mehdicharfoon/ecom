5b68cd73f83e9fa508de10ade8e876a50e4ef733
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-11-18 22:28:06 -0500

    built formButton component in formfields.js and added submit/login button to signinform

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-
-import { reduxForm, Field } from 'redux-form';
 import SignInForm from './signinForm';
 
 class SignIn extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
             <div className='sign-in'>
-                sign innn
                 <SignInForm className='sign-in__form' />
+                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
     }
 }
-
-SignIn = reduxForm({
-    form: 'SignIn'
-})(SignIn);
-
 export default SignIn;
