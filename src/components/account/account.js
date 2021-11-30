0a08df177abc340d6ae758b3139268923cd542de
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-11-30 10:06:23 -0500

    setup purchase history component

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-
 import AccountInformation from './accountInformation';
 import PurchaseHistory from './purchaseHistory';
-
 class Account extends Component {
-
     componentDidMount() {
-
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
         const navbarLinks = [
             {
                 _id: 0,
                 title: 'Purchase History',
                 active: false,
+                active: true,
                 component: <PurchaseHistory/>
             },
             {
                 _id: 1,
                 title: 'Account Information',
                 active: true,
+                active: false,
                 component: <AccountInformation/>
             }
         ]
-
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
-
     renderContent() {
-        return <h1> content </h1>
         let jsx;
         if(this.props.navbarLinks) {
             this.props.navbarLinks.forEach(link => {
@@ -44,18 +50,16 @@ class Account extends Component {
 
     render() {
         return (
-            <div>
+            
+            <div className='account'>
                 { this.renderContent() }
             </div>
         )
     }
 }
-
 function mapStateToProps(state) {
     const { headerLinks, navbarLinks } = state.headerNavbar;
     return { headerLinks, navbarLinks }
 }
-
 Account = connect(mapStateToProps, actions)(Account);
-
-export default Account; 
+export default Account;
