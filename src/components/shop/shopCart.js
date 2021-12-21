d3d334b77195a2b0969b6c0858a260b0b078223b
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-12-21 10:47:14 -0500

    built shop-cart-content-footer component and styled the checkout button

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -7,9 +7,9 @@ function CartButton({className, icon}) {
     )
 }
 
-function CartContent({className}) {
 function CartContent({className, products}) {
     let count = products.length;
+    let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
@@ -17,20 +17,36 @@ function CartContent({className, products}) {
             </div>
             <div className='cart-content__products'>
 
+                {productsJSX}
             </div>
             <div className='cart-content__footer'>
-
-            </div>
+            <CartFooter className='cart-content__footer' products={products}/>
         </div>
     )
 }
+
+function CartFooter({className, products}) {
+    const price = 7.96;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className='cart-footer__checkout'>
+                Checkout
+            </a>
+            <div className='cart-footer__subtotal'>
+                Subtotal
+            </div>
+            <div className='cart-footer__price'>
+                ${price}
+            </div>
+        </div>
+    )
+    }
 class ShopCart extends Component {
     render() {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content'/>
                 <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
             </div>
         )
