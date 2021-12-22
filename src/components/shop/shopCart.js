5f30c5f59f097e4b1e22c40d9e9f71a520169b84
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-12-22 10:09:25 -0500

    mapped cartproducts into cart product component instances

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,49 +1,67 @@
+src/components/shop/shopCart.js 
 import React, { Component } from 'react';
 import CartProduct from './cartProduct';
-
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-
 function CartButton({className, icon}) {
     return (
         <div className={`${className} cart-button`}>
-@@ -11,7 +14,7 @@ function CartButton({className, icon}) {
+            <i className={icon}/>        
+        </div>
+    )
+}
 
 function CartContent({className, products}) {
     let count = products.length;
-    let productsJSX = products.map(product => <CartProduct key={product}/>);
     let productsJSX = products.map(product => <CartProduct key={product._id}/>);
+    let productsJSX = products.map(product => <CartProduct {...product} key={product._id}/>);
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
-@@ -43,15 +46,29 @@ function CartFooter({className, products}) {
+                Cart ({count})
+            </div>
+            <div className='cart-content__products'>
+                {productsJSX}
+            </div>
+            <CartFooter className='cart-content__footer' products={products}/>
+        </div>
+    )
 }
-
+function CartFooter({className, products}) {
+    const price = 7.96;
+    return (
+        <div className={`${className} cart-footer`}>
+            <a className='cart-footer__checkout'>
+                Checkout
+            </a>
+            <div className='cart-footer__subtotal'>
+                Subtotal
+            </div>
+            <div className='cart-footer__price'>
+                ${price}
+            </div>
+        </div>
+    )
+}
 class ShopCart extends Component {
-
     componentDidMount() {
         this.props.fetchCartProducts();
     }
-
     render() {
         const { className } = this.props;
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554, 243, 3434, 4554, 3434, 4554, 243, 3434, 4554]}/>
                 <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
             </div>
         )
     }
 }
-
 function mapStateToProps(state) {
     const { cartProducts } = state.user;
     return {
         cartProducts
     }
 }
-
 ShopCart = connect(mapStateToProps, actions)(ShopCart);
-
 export default ShopCart;
