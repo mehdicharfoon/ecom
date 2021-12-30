978aa77b2713e8d66915109f40219b0aafb59710
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-12-30 12:19:55 -0500

    built order review products component

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+
 import { reduxForm, Field } from 'redux-form';
+
 import { FormButton } from '../formFields';
+
 import history from '../../history';
+
+import { FormButton } from '../formFields';
+import ReviewProducts from './reviewProducts';
+
 class ReviewForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
+
         return (
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
+                <ReviewProducts className='review-form__products'/>
                 <div className='review-form__line'></div>
                 <Field className='review-form__proceed'
-                onClick={() => history.push('/account')}
                 onClick={() => history.push('/signin')}
                 type='submit'
                 title='Proceed to Checkout'
                 name='proceed'
                 component={FormButton}/>
                 <Field className='review-form__back'
-                onClick={() => history.push('/signin')}
                 onClick={() => history.push('/shop')}
                 type='button'
                 title='Back'
