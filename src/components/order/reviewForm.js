09116f7678a3baa689386ba98047fc015a7c4319
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-12-30 11:22:24 -0500

    review form grid

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-
 import { reduxForm, Field } from 'redux-form';
-
 import { FormButton } from '../formFields';
-
 import history from '../../history';
-
 class ReviewForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-
         return (
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
                 <div className='review-form__line'></div>
                 <Field className='review-form__proceed'
                 onClick={() => history.push('/account')}
+                onClick={() => history.push('/signin')}
                 type='submit'
                 title='Proceed to Checkout'
                 name='proceed'
                 component={FormButton}/>
                 <Field className='review-form__back'
                 onClick={() => history.push('/signin')}
+                onClick={() => history.push('/shop')}
                 type='button'
                 title='Back'
                 name='back'
@@ -30,9 +27,7 @@ class ReviewForm extends Component {
         )
     }
 }
-
 ReviewForm = reduxForm({
     form: 'ReviewForm'
 })(ReviewForm);
-
 export default ReviewForm;
