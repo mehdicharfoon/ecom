319a26bd90abe61c1c7f0bb8094d7d0e416f7307
mohamed mehdi chorfi <mehdicharfoon@gmail.com>
2021-12-31 11:44:46 -0500

    built the information routes and container components payment and shipping js

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -3,11 +3,8 @@ import { reduxForm, Field } from 'redux-form';
 import history from '../../history';
 import { FormButton } from '../formFields';
 import ReviewProducts from './reviewProducts';
-
 class ReviewForm extends Component {
     render() {
-        const { className, handleSubmit } = this.props;
-
         const { className, handleSubmit, subtotal } = this.props;
         let tax = 0.16;
         return (
@@ -21,6 +18,7 @@ class ReviewForm extends Component {
                 <div className='review-form__line'></div>
                 <Field className='review-form__proceed'
                 onClick={() => history.push('/signin')}
+                onClick={() => history.push('/information/shipping')}
                 type='submit'
                 title='Proceed to Checkout'
                 name='proceed'
@@ -35,12 +33,10 @@ class ReviewForm extends Component {
                 <div className='review-form__details review-details'>
                     <div className='review-details__subtotal review-detail'>
                         <div className='review-detail__title'>Subtotal</div>
-                        <div className='review-detail__price'>$7.96</div>
                         <div className='review-detail__price'>${subtotal}</div>
                     </div>
                     <div className='review-details__subtotal review-detail'>
                         <div className='review-detail__title'>Tax</div>
-                        <div className='review-detail__price'>$0.16</div>
                         <div className='review-detail__price'>${tax}</div>
                     </div>
                     <div className='review-details__subtotal review-detail'>
@@ -49,10 +45,9 @@ class ReviewForm extends Component {
                     </div>
                     <div className='review-details__total review-detail review-detail-green'>
                         <div className='review-detail__title review-detail-green__title'>Total</div>
-                        <div className='review-detail__price review-detail-green__price'>$8.12</div>
                         <div className='review-detail__price review-detail-green__price'>${subtotal + tax}</div>
                     </div>
-
+ 
                 </div>
             </form>
         )
